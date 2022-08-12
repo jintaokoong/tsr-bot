8d92d38b49a997ec4674e9bd3bc7707bfb24f974
Jintao Koong <jintaokoong99@gmail.com>
2022-08-12 23:51:55 +0800

    feat: enhance logging update format

--- go.mod
+++ go.mod
@@ -6,3 +6,5 @@ require (
 	github.com/gempir/go-twitch-irc/v2 v2.8.1
 	github.com/joho/godotenv v1.4.0
 )
+
+require github.com/rs/xid v1.4.0 // indirect
--- main.go
+++ main.go
@@ -12,11 +12,12 @@ import (
 
 	"github.com/gempir/go-twitch-irc/v2"
 	"github.com/joho/godotenv"
+	"github.com/rs/xid"
 )
 
 type SRBody struct {
-	Name     string `json:"name"`
-	Audience string `json:"audience"`
+	Title     string `json:"title"`
+	Requester string `json:"requester"`
 }
 
 func main() {
@@ -50,6 +51,8 @@ func main() {
 		lg.Println("reconnected")
 	})
 	client.OnPrivateMessage(func(message twitch.PrivateMessage) {
+		rid := xid.New()
+		rtag := fmt.Sprintf("[%s]", rid.String())
 		exp := "^(點:|點：).*"
 		r, _ := regexp.Compile(exp)
 		m := r.FindStringSubmatch(message.Message)
@@ -59,18 +62,22 @@ func main() {
 		c := m[1]
 		sr := strings.Replace(message.Message, c, "", 1)
 		sr = strings.Trim(sr, " ")
-		lg.Println("captured:", sr)
+		lg.Println(rtag, "captured:", sr)
+		if len(sr) == 0 {
+			log.Println(rtag, "empty title")
+			return
+		}
 		rq := SRBody{
-			Name:     sr,
-			Audience: message.User.DisplayName,
+			Title:     sr,
+			Requester: message.User.DisplayName,
 		}
 		bt, err := json.Marshal(rq)
 		if err != nil {
-			lg.Panicln(err)
+			lg.Panicln(rtag, err)
 		}
 		req, err := http.NewRequest("POST", baseUrl+"/api/requests", bytes.NewBuffer(bt))
 		if err != nil {
-			lg.Println(err.Error())
+			lg.Println(rtag, err.Error())
 			return
 		}
 		req.Header.Set("content-type", "application/json")
@@ -78,13 +85,13 @@ func main() {
 
 		resp, err := http.DefaultClient.Do(req)
 		if err != nil {
-			lg.Println(err.Error())
-			lg.Printf("failed to add %s for user %s\n", sr, message.User.DisplayName)
+			lg.Println(rtag, err.Error())
+			lg.Printf("%s failed to add %s for user %s\n", rtag, sr, message.User.DisplayName)
 		} else if resp.StatusCode != 200 {
 			lg.Println(resp.StatusCode)
-			lg.Printf("failed to add %s for user %s\n", sr, message.User.DisplayName)
+			lg.Printf("%s failed to add %s for user %s\n", rtag, sr, message.User.DisplayName)
 		} else {
-			lg.Printf("%s added to list for user %s\n", sr, message.User.DisplayName)
+			lg.Printf("%s %s added to list for user %s\n", rtag, sr, message.User.DisplayName)
 			client.Say(message.Channel, fmt.Sprintf("%s 成功點了 %s", message.User.DisplayName, sr))
 		}
 	})
